c0a2c8f1d68d74b3f80d5fd3c871946dd546cef4
JCAT <34417384+yaolaotou@users.noreply.github.com>
2018-06-19 21:53:40 +0800

    2.0
    1.导航已完成。
    2.设计图（基本全参考杉果）

--- index.php
+++ index.php
@@ -4,25 +4,34 @@
 <meta charset="utf-8">
 <title>首页</title>
 <style type="text/css">
+#bg{
+		width:1200px;
+		height:1000px;
+		background:#DCD7D6;
+		margin:auto;
+		padding:auto;}
 	#logo{
 		width: 80px;
 		height: 80px;
 		background-image:url(img/logo.png);
 	}
 	#nav{
-		width:1000px;
-		height: 100px;
-		background-color: aquamarine;
+		width:1100px;
+		height: 80px;
+		background-color:#FFF;
 		position: relative;
+		margin:auto;
+		padding:auto;
 	}
 	</style>
 </head>
 
 <body>
+<div id="bg">
 <div id="nav">
 	<div id="logo"></div>
 </div>
-
+</div>
 <?php
 echo "hello world"	
 ?>
