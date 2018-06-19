402db98aecb8ed1c60a957b5b181265cc019da7e
JCAT <34417384+yaolaotou@users.noreply.github.com>
2018-06-19 20:36:52 +0800

    1.0
    1.0

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>首页</title>
+<style type="text/css">
+	#logo{
+		width: 80px;
+		height: 80px;
+		background-image:url(img/logo.png);
+	}
+	#nav{
+		width:1000px;
+		height: 100px;
+		background-color: aquamarine;
+		position: relative;
+	}
+	</style>
+</head>
+
+<body>
+<div id="nav">
+	<div id="logo"></div>
+</div>
+
+<?php
+echo "hello world"	
+?>
+</body>
+</html>
